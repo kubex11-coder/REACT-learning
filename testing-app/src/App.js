c4f9381e34fb7486455fef4ba18e87ca31ff0cc6
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-03-23 18:09:31 +0100

    vytvořen window resize

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react"
+
 const App = () => {
-    return <h1>test bitch</h1>
+    const [windowSize, setWindowSize] = useState(window.innerWidth)
+    const sizeControl = () => {
+        setWindowSize(window.innerWidth)
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", sizeControl)
+    })
+
+    return (
+        <div>
+            <h1>Šířka okna</h1>
+            <h2>{windowSize}</h2>
+        </div>
+    )
 }
 
 export default App
