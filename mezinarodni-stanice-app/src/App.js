115d4b23ac8ac0547486e0534ae15f803b414e85
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-03-26 16:18:32 +0200

    fetch data pro aplikaci a jejich prevod do json formátu

--- mezinarodni-stanice-app/src/App.js
+++ mezinarodni-stanice-app/src/App.js
@@ -1,5 +1,16 @@
 const App = () => {
-    return <h1>test</h1>
+    const URL = "http://api.open-notify.org/iss-now.json"
+
+    const getCoordinates = async function () {
+        const response = await fetch(URL)
+        const data = await response.json()
+        console.log(data.iss_position.longitude)
+        console.log(data.iss_position.latitude)
+    }
+
+    getCoordinates()
+
+    return <h1>API</h1>
 }
 
 export default App
