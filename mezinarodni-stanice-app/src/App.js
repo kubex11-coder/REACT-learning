dbbeb73b61e498045088457bb9f50e137e917547
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-03-26 18:03:09 +0200

    dokončení aplikace mezinarodní stanice

--- mezinarodni-stanice-app/src/App.js
+++ mezinarodni-stanice-app/src/App.js
@@ -1,16 +1,38 @@
+import { useState, useEffect } from "react"
+
 const App = () => {
     const URL = "http://api.open-notify.org/iss-now.json"
+    const [latitude, setLatitude] = useState("")
+    const [longitude, setLongitude] = useState("")
+    const [urlMap, setUrlMap] = useState("")
 
     const getCoordinates = async function () {
         const response = await fetch(URL)
         const data = await response.json()
-        console.log(data.iss_position.longitude)
-        console.log(data.iss_position.latitude)
+
+        setLatitude(data.iss_position.latitude)
+        setLongitude(data.iss_position.longitude)
+        const iss_long = data.iss_position.longitude
+        const iss_lat = data.iss_position.latitude
+        setUrlMap(`https://mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}&z=11`)
     }
 
-    getCoordinates()
+    useEffect(() => {
+        getCoordinates()
+    }, [])
 
-    return <h1>API</h1>
+    return (
+        <div>
+            <h1>API</h1>
+            <h2>Zeměpisná šířka</h2>
+            <p>{latitude}</p>
+            <h2>Zeměpisná délka</h2>
+            <p>{longitude}</p>
+            <a href={urlMap} target="_blank">
+                Pozice ISSv mapách
+            </a>
+        </div>
+    )
 }
 
 export default App
