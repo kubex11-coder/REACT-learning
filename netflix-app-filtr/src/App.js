aec0ddb82d8b144781eccd9f45aff8c35544c9b6
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-04-14 18:19:11 +0200

    funkčnost aplikace - netflix-app-filtr

--- netflix-app-filtr/src/App.js
+++ netflix-app-filtr/src/App.js
@@ -1,9 +1,47 @@
 import "./index.css"
+import { useState } from "react"
 import allMovies from "./data"
 import categories from "./categories"
 
 const App = () => {
-    return <h1>test</h1>
+    const [typeOfMovie, setTypeOfMovie] = useState("komedie")
+
+    const vysledneFilmy = allMovies.filter((oneMovie) => {
+        return oneMovie.category === typeOfMovie
+    })
+
+    return (
+        <div>
+            <div>
+                {categories.map((oneCategory, index) => {
+                    return (
+                        <button
+                            key={index}
+                            onClick={() => setTypeOfMovie(oneCategory)}
+                        >
+                            {oneCategory}
+                        </button>
+                    )
+                })}
+            </div>
+            <div>
+                {vysledneFilmy.map((oneMovie) => {
+                    const { id, image, title, age, tags, description } =
+                        oneMovie
+
+                    return (
+                        <div key={id}>
+                            <img src={image} alt="" />
+                            <h2>{title}</h2>
+                            <p>{age}</p>
+                            <p>{tags}</p>
+                            <p>{description}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default App
